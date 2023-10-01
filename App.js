6e3e97e038c18f23b704ef56f89800c1320e129d
Niranjan Powade <niranjanpowadex@gmail.com>
2023-10-01 19:29:26 +0530

    Adding ScrollBar

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, TouchableOpacity, Text, Image } from "react-native";
+import { View, TouchableOpacity, Text, Image, ScrollView } from "react-native";
 import Menu from "./src/Menu";
 import NewsDetail from "./src/NewsDetail";
 
@@ -55,37 +55,38 @@ export default function App() {
   };
 
   return (
-    <View style={{ flex: 1 }}>
-      <View
-        style={{
-          backgroundColor: "violet",
-          height: 60,
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-          paddingHorizontal: 10,
-          paddingTop: 10,
-        }}
-      >
-        <View style={{ flex: 1, alignItems: "flex-start" }}>
-          <TouchableOpacity onPress={toggleMenu}>
-            <Text style={{ color: "white", fontSize: 20 }}>{"\u2630"}</Text>
-          </TouchableOpacity>
+    <ScrollView style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
+        <View
+          style={{
+            backgroundColor: "violet",
+            height: 60,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            paddingHorizontal: 10,
+            paddingTop: 10,
+          }}
+        >
+          <View style={{ flex: 1, alignItems: "flex-start" }}>
+            <TouchableOpacity onPress={toggleMenu}>
+              <Text style={{ color: "white", fontSize: 20 }}>{"\u2630"}</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={{ flex: 1, alignItems: "center" }}>
+            <Text style={{ color: "white", fontSize: 20, fontWeight: "bold" }}>
+              NewsIn45
+            </Text>
+          </View>
+          <View style={{ flex: 1, alignItems: "flex-end" }}>
+            <TouchableOpacity onPress={() => console.log("Help Pressed")}>
+              <Text style={{ color: "white", fontSize: 20 }}>{"\u003F"}</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <View style={{ flex: 1, alignItems: "center" }}>
-          <Text style={{ color: "white", fontSize: 20, fontWeight: "bold" }}>
-            NewsIn45
-          </Text>
-        </View>
-        <View style={{ flex: 1, alignItems: "flex-end" }}>
-          <TouchableOpacity onPress={() => console.log("Help Pressed")}>
-            <Text style={{ color: "white", fontSize: 20 }}>{"\u003F"}</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
 
-      {/* List of News Items */}
-      {/* {newsData.map((newsItem) => (
+        {/* List of News Items */}
+        {/* {newsData.map((newsItem) => (
         <TouchableOpacity
           key={newsItem.id}
           onPress={() => openNewsDetail(newsItem)}
@@ -101,46 +102,47 @@ export default function App() {
           <Text>{newsItem.description}</Text>
         </TouchableOpacity>
       ))} */}
-      {newsData.map((newsItem) => (
-        <TouchableOpacity
-          key={newsItem.id}
-          onPress={() => openNewsDetail(newsItem)}
-          style={{
-            borderBottomWidth: 1,
-            borderColor: "lightgray",
-            padding: 12,
-          }}
-        >
-          <View
+        {newsData.map((newsItem) => (
+          <TouchableOpacity
+            key={newsItem.id}
+            onPress={() => openNewsDetail(newsItem)}
             style={{
-              flexDirection: "column", // Stack image and description vertically
-              alignItems: "center", // Center content horizontally
+              borderBottomWidth: 1,
+              borderColor: "lightgray",
+              padding: 12,
             }}
           >
-            <Image
-              source={{ uri: newsItem.image }}
+            <View
               style={{
-                width: 200, // Adjust image width as needed
-                height: 150, // Adjust image height as needed
-                marginBottom: 12, // Add some spacing between image and description
-                borderRadius: 10, // Add border radius for card-like appearance
+                flexDirection: "column", // Stack image and description vertically
+                alignItems: "center", // Center content horizontally
               }}
-            />
-            <Text style={{ fontSize: 18, fontWeight: "bold" }}>
-              {newsItem.title}
-            </Text>
-            <Text>{newsItem.description}</Text>
-          </View>
-        </TouchableOpacity>
-      ))}
+            >
+              <Image
+                source={{ uri: newsItem.image }}
+                style={{
+                  width: 200, // Adjust image width as needed
+                  height: 150, // Adjust image height as needed
+                  marginBottom: 12, // Add some spacing between image and description
+                  borderRadius: 10, // Add border radius for card-like appearance
+                }}
+              />
+              <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+                {newsItem.title}
+              </Text>
+              <Text>{newsItem.description}</Text>
+            </View>
+          </TouchableOpacity>
+        ))}
 
-      {/* Menu Component */}
-      <Menu isVisible={isMenuVisible} onClose={toggleMenu} />
+        {/* Menu Component */}
+        <Menu isVisible={isMenuVisible} onClose={toggleMenu} />
 
-      {/* News Detail Component */}
-      {selectedNews && (
-        <NewsDetail newsItem={selectedNews} onClose={closeNewsDetail} />
-      )}
-    </View>
+        {/* News Detail Component */}
+        {selectedNews && (
+          <NewsDetail newsItem={selectedNews} onClose={closeNewsDetail} />
+        )}
+      </View>
+    </ScrollView>
   );
 }
