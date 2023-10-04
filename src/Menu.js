938e7b4f9f068b90f1015953558657568e9d3b5c
Niranjan Powade <niranjanpowadex@gmail.com>
2023-10-04 22:55:29 +0530

    Improve NewsDetailScreen and MenuScreen UI.

--- src/Menu.js
+++ src/Menu.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, TouchableOpacity, Modal } from "react-native";
+import { View, Text, TouchableOpacity, Modal, StyleSheet } from "react-native";
 
 const Menu = ({ isVisible, onClose }) => {
   return (
@@ -9,37 +9,44 @@ const Menu = ({ isVisible, onClose }) => {
       visible={isVisible}
       onRequestClose={onClose}
     >
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "rgba(0,0,0,0.5)",
-        }}
-      >
-        <View
-          style={{
-            backgroundColor: "white",
-            padding: 20,
-            borderRadius: 10,
-          }}
-        >
+      <View style={styles.modalBackground}>
+        <View style={styles.menuContainer}>
           <TouchableOpacity onPress={() => console.log("Sign In Pressed")}>
-            <Text>Sign In</Text>
+            <Text style={styles.menuItem}>Sign In</Text>
           </TouchableOpacity>
           <TouchableOpacity onPress={() => console.log("Language Pressed")}>
-            <Text>Language</Text>
+            <Text style={styles.menuItem}>Language</Text>
           </TouchableOpacity>
           <TouchableOpacity onPress={() => console.log("Text Size Pressed")}>
-            <Text>Text Size</Text>
+            <Text style={styles.menuItem}>Text Size</Text>
           </TouchableOpacity>
           <TouchableOpacity onPress={() => console.log("Help Pressed")}>
-            <Text>Help</Text>
+            <Text style={styles.menuItem}>Help</Text>
           </TouchableOpacity>
         </View>
       </View>
     </Modal>
   );
 };
+
+const styles = StyleSheet.create({
+  modalBackground: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  menuContainer: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+  },
+  menuItem: {
+    fontSize: 18,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    color: "#333",
+  },
+});
 
 export default Menu;
--- src/NewsDetailScreen.js
+++ src/NewsDetailScreen.js
@@ -1,24 +1,41 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 
 const NewsDetailScreen = ({ route }) => {
   const { newsItem } = route.params;
 
   return (
-    <View style={{ flex: 1, backgroundColor: "white", padding: 16 }}>
-      <Image
-        source={{ uri: newsItem.image }}
-        style={{
-          width: "100%",
-          height: 200,
-          marginBottom: 12,
-          borderRadius: 10,
-        }}
-      />
-      <Text style={{ fontSize: 18, fontWeight: "bold" }}>{newsItem.title}</Text>
-      <Text>{newsItem.description}</Text>
+    <View style={styles.container}>
+      <Image source={{ uri: newsItem.image }} style={styles.image} />
+      <Text style={styles.title}>{newsItem.title}</Text>
+      <Text style={styles.description}>{newsItem.description}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    padding: 16,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    marginBottom: 12,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 8,
+    color: "#333",
+  },
+  description: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: "#555",
+  },
+});
 
 export default NewsDetailScreen;
